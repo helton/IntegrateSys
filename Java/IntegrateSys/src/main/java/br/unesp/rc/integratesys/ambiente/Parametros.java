a478dedd04b6e98b37ad4af19250228c8a33a241
Waldeilson Eder dos Santos <wwesantos@gmail.com>
2014-07-03 18:20:09 -0300

    Parametros - armazenamento parte 2

--- Java/IntegrateSys/src/main/java/br/unesp/rc/integratesys/ambiente/Parametros.java
+++ Java/IntegrateSys/src/main/java/br/unesp/rc/integratesys/ambiente/Parametros.java
@@ -11,179 +11,54 @@ package br.unesp.rc.integratesys.ambiente;
  */
 public class Parametros {
 
-    private int temperaturaInicial;
-    private int umidadeInicial;
-    private int luminosidadeInicial;
-    private int temperaturaIdeal;
-    private int umidadeIdeal;
-    private int luminosidadeIdeal;
-    private int temperaturaMinimaCritica;
-    private int temperaturaMaximaCritica;
-    private int umidadeMinimaCritica;
-    private int umidadeMaximaCritica;
-    private int luminosidadeMinimaCritica;
-    private int luminosidadeMaximaCritica;
-
-    public Parametros() {
-        temperaturaInicial = ParametrosArmazenados.getTemperaturaInicial();
-        umidadeInicial = ParametrosArmazenados.getUmidadeInicial();
-        luminosidadeInicial = ParametrosArmazenados.getLuminosidadeInicial();
-        temperaturaIdeal = ParametrosArmazenados.getTemperaturaIdeal();
-        umidadeIdeal = ParametrosArmazenados.getUmidadeIdeal();
-        luminosidadeIdeal = ParametrosArmazenados.getLuminosidadeIdeal();
-        temperaturaMinimaCritica = ParametrosArmazenados.getTemperaturaMinimaCritica();
-        temperaturaMaximaCritica = ParametrosArmazenados.getTemperaturaMaximaCritica();
-        umidadeMinimaCritica = ParametrosArmazenados.getUmidadeMinimaCritica();
-        umidadeMaximaCritica = ParametrosArmazenados.getUmidadeMaximaCritica();
-        luminosidadeMinimaCritica = ParametrosArmazenados.getLuminosidadeMinimaCritica();
-        luminosidadeMaximaCritica = ParametrosArmazenados.getLuminosidadeMaximaCritica();
+    public Parametros() {                
     }
 
-    /**
-     * @return the temperaturaIdeal
-     */
-    public int getTemperaturaIdeal() {
-        return temperaturaIdeal;
-    }
-
-    /**
-     * @return the umidadeIdeal
-     */
-    public int getUmidadeIdeal() {
-        return umidadeIdeal;
-    }
-
-    /**
-     * @return the luminosidadeIdeal
-     */
-    public int getLuminosidadeIdeal() {
-        return luminosidadeIdeal;
-    }
-
-    /**
-     * @return the temperaturaMinimaCritica
-     */
-    public int getTemperaturaMinimaCritica() {
-        return temperaturaMinimaCritica;
-    }
-
-    /**
-     * @return the temperaturaMaximaCritica
-     */
-    public int getTemperaturaMaximaCritica() {
-        return temperaturaMaximaCritica;
-    }
-
-    /**
-     * @return the umidadeMinimaCritica
-     */
-    public int getUmidadeMinimaCritica() {
-        return umidadeMinimaCritica;
-    }
-
-    /**
-     * @return the umidadeMaximaCritica
-     */
-    public int getUmidadeMaximaCritica() {
-        return umidadeMaximaCritica;
-    }
-
-    /**
-     * @return the luminosidadeMinimaCritica
-     */
-    public int getLuminosidadeMinimaCritica() {
-        return luminosidadeMinimaCritica;
-    }
-
-    /**
-     * @return the luminosidadeMaximaCritica
-     */
-    public int getLuminosidadeMaximaCritica() {
-        return luminosidadeMaximaCritica;
-    }
-
-    /**
-     * @param temperaturaIdeal the temperaturaIdeal to set
-     */
-    public void setTemperaturaIdeal(int temperaturaIdeal) {
-        this.temperaturaIdeal = temperaturaIdeal;
-    }
-
-    /**
-     * @param umidadeIdeal the umidadeIdeal to set
-     */
-    public void setUmidadeIdeal(int umidadeIdeal) {
-        this.umidadeIdeal = umidadeIdeal;
-    }
-
-    /**
-     * @param luminosidadeIdeal the luminosidadeIdeal to set
-     */
-    public void setLuminosidadeIdeal(int luminosidadeIdeal) {
-        this.luminosidadeIdeal = luminosidadeIdeal;
-    }
-
-    /**
-     * @param temperaturaMinimaCritica the temperaturaMinimaCritica to set
-     */
-    public void setTemperaturaMinimaCritica(int temperaturaMinimaCritica) {
-        this.temperaturaMinimaCritica = temperaturaMinimaCritica;
-    }
-
-    /**
-     * @param temperaturaMaximaCritica the temperaturaMaximaCritica to set
-     */
-    public void setTemperaturaMaximaCritica(int temperaturaMaximaCritica) {
-        this.temperaturaMaximaCritica = temperaturaMaximaCritica;
-    }
-
-    /**
-     * @param umidadeMinimaCritica the umidadeMinimaCritica to set
-     */
-    public void setUmidadeMinimaCritica(int umidadeMinimaCritica) {
-        this.umidadeMinimaCritica = umidadeMinimaCritica;
-    }
-
-    /**
-     * @param umidadeMaximaCritica the umidadeMaximaCritica to set
-     */
-    public void setUmidadeMaximaCritica(int umidadeMaximaCritica) {
-        this.umidadeMaximaCritica = umidadeMaximaCritica;
-    }
-
-    /**
-     * @param luminosidadeMinimaCritica the luminosidadeMinimaCritica to set
-     */
-    public void setLuminosidadeMinimaCritica(int luminosidadeMinimaCritica) {
-        this.luminosidadeMinimaCritica = luminosidadeMinimaCritica;
-    }
-
-    /**
-     * @param luminosidadeMaximaCritica the luminosidadeMaximaCritica to set
-     */
-    public void setLuminosidadeMaximaCritica(int luminosidadeMaximaCritica) {
-        this.luminosidadeMaximaCritica = luminosidadeMaximaCritica;
-    }
-
-    /**
-     * @return the temperaturaInicial
-     */
     public int getTemperaturaInicial() {
-        return temperaturaInicial;
+        return ParametrosArmazenados.getTemperaturaInicial();
     }
 
-    /**
-     * @return the umidadeInicial
-     */
     public int getUmidadeInicial() {
-        return umidadeInicial;
+        return ParametrosArmazenados.getUmidadeInicial();
     }
 
-    /**
-     * @return the luminosidadeInicial
-     */
     public int getLuminosidadeInicial() {
-        return luminosidadeInicial;
+        return ParametrosArmazenados.getLuminosidadeInicial();
     }
 
+    public int getTemperaturaIdeal() {
+        return ParametrosArmazenados.getTemperaturaIdeal();
+    }
+
+    public int getUmidadeIdeal() {
+        return ParametrosArmazenados.getUmidadeIdeal();
+    }
+
+    public int getLuminosidadeIdeal() {
+        return ParametrosArmazenados.getLuminosidadeIdeal();
+    }
+
+    public int getTemperaturaMinimaCritica() {
+        return ParametrosArmazenados.getTemperaturaMinimaCritica();
+    }
+
+    public int getTemperaturaMaximaCritica() {
+        return ParametrosArmazenados.getTemperaturaMaximaCritica();
+    }
+
+    public int getUmidadeMinimaCritica() {
+        return ParametrosArmazenados.getUmidadeMinimaCritica();
+    }
+
+    public int getUmidadeMaximaCritica() {
+        return ParametrosArmazenados.getUmidadeMaximaCritica();
+    }
+
+    public int getLuminosidadeMinimaCritica() {
+        return ParametrosArmazenados.getLuminosidadeMinimaCritica();
+    }
+
+    public int getLuminosidadeMaximaCritica() {
+        return ParametrosArmazenados.getLuminosidadeMaximaCritica();
+    }    
 }
